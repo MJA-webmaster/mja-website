0818cb6e68866dc9ba352c29a40f88d77ce6470b
MJA-webmaster <maldivesjournalistassociation@proton.me>
2026-05-27 01:01:36 +0500

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -55,7 +55,7 @@ export default function Nav() {
           <img
             src="/mjalogo.png"
             alt="Maldives Journalists Association"
-            className="h-20 w-auto"
+            className="h-16 w-auto"
           />
         </Link>
 

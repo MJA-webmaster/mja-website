3344332379607b1fd09c3384c2285744a2139ef8
mja <hshazil@gmail.com>
2026-09-03 08:51:47 +0500

    fix: pdfjs worker use local file instead of CDN

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -50,7 +50,7 @@ export default function FileUpload({
   async function generatePdfCover(file: File): Promise<Blob | null> {
     try {
       const pdfjsLib = await import('pdfjs-dist')
-      pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`
+      pdfjsLib.GlobalWorkerOptions.workerSrc = new URL("pdfjs-dist/build/pdf.worker.min.mjs", import.meta.url).toString()
 
       const arrayBuffer = await file.arrayBuffer()
       const pdf = await pdfjsLib.getDocument({ data: arrayBuffer }).promise

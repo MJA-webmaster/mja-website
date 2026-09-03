54da1a88dbd832ff21ca89fe7e7c725b35b6d96d
MJA-webmaster <maldivesjournalistassociation@proton.me>
2026-09-03 20:38:54 +0500

    Update page.tsx

--- app/members-directory/page.tsx
+++ app/members-directory/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation'
 
 export default function MembersDirectoryPage() {
-  redirect('/members-directory/category-one')
+  redirect('/members-directory/professional')
 }
